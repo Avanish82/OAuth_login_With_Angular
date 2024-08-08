dc310ba139f6acd8d91b77d1e1ae1f66a0514bef
AvanishKumarSingh1 <avanish.as@gmail.com>
2024-08-08 20:15:05 +0530

    first
    commit

---
+++ OAuth2-SpringSecurity-Angular-master/src/main/java/com/spring/oauthdemo/SpringSecurityOAuthDemoApplication.java
@@ -0,0 +1,18 @@
+package com.spring.oauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAuthDemoApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringSecurityOAuthDemoApplication.class, args);
+    }
+    /**
+     * 
+     * source Url =https://pavankjadda.dev/oauth2-0-authentication-with-spring-security-angular-and-mysql/
+     */
+}
+
